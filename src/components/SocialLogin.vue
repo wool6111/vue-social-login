46354d1d72147b1e4f02891085fbd781cb4135e6
양명모 <wool6111@gmail.com>
2022-11-30 17:52:26 +0900

    delete

--- src/components/SocialLogin.vue
+++ src/components/SocialLogin.vue
@@ -1,7 +1,17 @@
 <template>
-	<div>Social Login</div>
+	<h1>Naver</h1>
+	<div>
+		<div id="naver_id_login"></div>
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const naver_id_login = new window.naver_id_login('Client Id', 'callback URL');
+const state = naver_id_login.getUniqState();
+naver_id_login.setButton('white', 2, 40); // 버튼 설정
+naver_id_login.setState(state);
+// naver_id_login.setPopup(); // popup 설정을 위한 코드
+naver_id_login.init_naver_id_login();
+</script>
 
 <style scoped></style>
